b1f9199c8b0b141dc3a5c1fd12fefa716453876a
Anton Pashkevich <mario.lazer@mail.ru>
2020-09-15 17:20:38 +0300

    initialized solution

---
+++ FakerConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FakerDTO;
+
+namespace FakerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var faker = new Faker();
+            Example obj = faker.Create<Example>();
+            Console.WriteLine("int property: {0}, double property: {1}, string property: {2}", obj.IntNumber, obj.DoubleNumber, obj.StringItem);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Example
+    {
+        public string StringItem { get; set; }
+        public int IntNumber { get; set; }
+        public double DoubleNumber { get; set; }
+    }
+}
---
+++ FakerDTO/Faker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakerDTO
+{
+    public class Faker
+    {
+        public T Create<T>()
+        {
+            Type TargetType = typeof(T);
+            var TargetObject = Activator.CreateInstance(TargetType);
+            MemberInfo[] members =  TargetType.GetMembers();
+            foreach (MemberInfo member in members)
+            {
+                if (member.MemberType == MemberTypes.Property)
+                {
+                    if (((PropertyInfo)member).PropertyType == typeof(int))
+                        ((PropertyInfo)member).SetValue(TargetObject, 2);
+                    else if (((PropertyInfo)member).PropertyType == typeof(double))
+                        ((PropertyInfo)member).SetValue(TargetObject, 13.42);
+                    else if (((PropertyInfo)member).PropertyType == typeof(string))
+                        ((PropertyInfo)member).SetValue(TargetObject, "example string");
+                }
+                else
+                {
+                    ;
+                }
+            }
+
+            return (T)TargetObject;
+        }
+    }
+}
